945748c9e18f159a2af3235954c24e03c5063afb
basmaks <143968316+basmaks@users.noreply.github.com>
2023-09-14 03:54:21 +0300

    Add files via upload

---
+++ DatasetDescription.R
@@ -0,0 +1,42 @@
+***
+  "Dataset Description"
+***
+  
+  ## Dataset Goal 
+  
+Our goal of collecting this dataset is to build a model capable of predicting 
+the potential success of startups.
+
+## Dataset Source 
+
+https://www.kaggle.com/datasets/manishkc06/startup-success-prediction
+
+## General Information 
+
+Number of objects: 925 rows
+
+Number of attributes: 20 columns
+1.	name
+2.	city
+3.	founded_at
+4.	close_at
+5.	first_funding_at
+6.	last_funding_at
+7.	age_first_funding_year
+8.	age_first_milestone_year
+9.	funding_rounds
+10.	funding_total_usd
+11.	category_code
+12.	has_VC
+13.	has_angel
+14.	has_roundA
+15.	has_roundB
+16.	has_roundC
+17.	has_roundD
+18.	avg_participants
+19.	is_top500
+20.	status
+
+In this dataset, the class label we are focusing on is "status," 
+which distinguishes between successful startups (labeled as "acquired") 
+and unsuccessful ones (labeled as "closed").
